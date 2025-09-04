26d7740b7f5aa2faa92dcf934b4ae11199b6ee83
iliailia12 <iliakviciani392@gmail.com>
2025-09-04 22:33:41 +0400

    second commit

---
+++ Day 145/Homework/task2/src/Card.jsx
@@ -0,0 +1,16 @@
+// src/components/Card.jsx
+import React from 'react';
+import './Card.css';
+
+function Card() {
+  return (
+    <div className="card">
+      <h2 className="card-title">Სათაური</h2>
+      <p className="card-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam esse corporis tempora beatae exercitationem provident magnam in vel enim, facere, veritatis, maiores laudantium numquam? Fuga asperiores corporis neque sed iusto?
+      </p>
+    </div>
+  );
+}
+
+export default Card;
